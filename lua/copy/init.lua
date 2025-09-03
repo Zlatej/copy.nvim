56edd5d5e6ead5652f3902bba2e06e9d09507cfa
Matej Skalicky <matej.skalickyy@gmail.com>
2025-09-03 20:19:39 +0200

    idk

--- lua/copy/init.lua
+++ lua/copy/init.lua
@@ -6,14 +6,22 @@ function M.setup(opts)
 	local config = require("copy.config")
 	cfg = config.setup(opts)
 
-	vim.api.nvim_create_user_command("Copy line with context", M.copy_line, {})
-	vim.api.nvim_create_user_command("Copy context", M.copy_context, {})
-	vim.keymap.set("n", cfg.keymap.cp_line, M.copy_line, { desc = "Copies line with context" })
-	vim.keymap.set("n", cfg.keymap.cp_visual, M.copy_context, { desc = "Copies context" })
+	-- Line
+	vim.api.nvim_create_user_command("CopyLine", M.copy_line, {})
+	vim.keymap.set("n", cfg.keymap.cp_line, M.copy_line, { desc = "Copy line with context" })
+	-- Context
+	vim.api.nvim_create_user_command("CopyContext", M.copy_context, {})
+	vim.keymap.set("n", cfg.keymap.cp_context, M.copy_context, { desc = "Copy context" })
+	-- Visual
+	-- vim.api.nvim_create_user_command("CopyVisual", M.copy_selection, {})
+	-- vim.keymap.set("v", cfg.keymap.cp_visual, M.copy_selection, { desc = "Copy visual selection" })
 end
 
 function M.copy_line()
 	local line = vim.api.nvim_get_current_line()
+	if cfg.remove_indent then
+		line = TrimIndent(line)
+	end
 	M.copy(line)
 end
 
@@ -21,19 +29,16 @@ function M.copy_context()
 	M.copy("")
 end
 
----@param selection string|string[]
+---@param selection string
 function M.copy(selection)
 	local file = vim.fn.expand("%:p")
-	if cfg.prefix and file:sub(1, #cfg.prefix) == cfg.prefix then
+	if #cfg.prefix ~= 0 and file:sub(1, #cfg.prefix) == cfg.prefix then
 		file = file:sub(#cfg.prefix + 1)
 	end
 	local line_num = vim.fn.line(".")
 
 	local res
 	if #selection ~= 0 then
-		if cfg.remove_indent then
-			selection = TrimIndent(selection)
-		end
 		res = string.format("%s:%d\n%s", file, line_num, selection)
 	else
 		res = string.format("%s:%d", file, line_num)
@@ -43,6 +48,8 @@ function M.copy(selection)
 	print("Copied line " .. line_num .. ": " .. selection)
 end
 
+---@param str string
+---@return string
 function TrimIndent(str)
 	return str:match("^%s*(.-)%s*$")
 end
