38180a3f6e8cd009302e1a612652511ed77b3152
Matej Skalicky <matej.skalickyy@gmail.com>
2025-09-24 22:03:38 +0200

    debug logging

---
+++ lua/copy/log.lua
@@ -0,0 +1,28 @@
+local M = {}
+local DEBUG = true
+local log_file = vim.fn.stdpath("data") .. "/copy_nvim_debug.log"
+
+function M.debug(...)
+	if not DEBUG then
+		return
+	end
+
+	local args = { ... }
+	local message = table.concat(
+		vim.tbl_map(function(arg)
+			return type(arg) == "table" and vim.inspect(arg) or tostring(arg)
+		end, args),
+		" "
+	)
+
+	local timestamp = os.date("%Y-%m-%d %H:%M:%S")
+	local log_entry = string.format("[%s] %s\n", timestamp, message)
+
+	local file = io.open(log_file, "a")
+	if file then
+		file:write(log_entry)
+		file:close()
+	end
+end
+
+return M
